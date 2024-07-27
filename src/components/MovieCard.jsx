26599e416f953ceca4bc87a8fe1a630c58f79a09
Bappy Modok <bappymodok0011@gmail.com>
2024-07-27 10:44:46 +0600

    image ratting added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getImgUrl } from "../utils/chine-utlity";
+import Rating from "./Rating";
 
 const MovieCard = ({ movie }) => {
   console.log(movie);
@@ -10,11 +11,7 @@ const MovieCard = ({ movie }) => {
         <h3 className="text-xl mb-1">{movie.title}</h3>
         <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
         <div className="flex items-center space-x-1 mb-5">
-          <img src="./assets/star.svg" width="14" height="14" alt="" />
-          <img src="./assets/star.svg" width="14" height="14" alt="" />
-          <img src="./assets/star.svg" width="14" height="14" alt="" />
-          <img src="./assets/star.svg" width="14" height="14" alt="" />
-          <img src="./assets/star.svg" width="14" height="14" alt="" />
+          <Rating value={movie.rating} />
         </div>
         <a
           className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm mb-0"
