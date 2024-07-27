916d5650ec2f45dab31c0b6619c54e400673d41f
Bappy Modok <bappymodok0011@gmail.com>
2024-07-27 09:33:37 +0600

    movie List and footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Headers from "./components/Headers";
 import Sidebar from "./components/Sidebar";
+import MoviesList from "./components/MoviesList";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Headers />
       <main>
-        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sidebar/>
+          <MoviesList/>
         </div>
       </main>
+      <Footer/>
     </div>
   );
 };
